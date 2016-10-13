0183754b0250f530b0cdff1b2e6edae3f8ce731e
Hjálmar Diego Arnórsson <hjalmar15@ru.is>
2016-10-13 19:21:35 +0000

    Code refined to a single split function for both comma and \n. All tests passed

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,12 +6,8 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
+		else if(text.contains(",") || text.contains("\n")){
 			return sum(splitNumbers(text));
-		}
-		else if(text.contains("\n"))
-		{
-			return sum(splitNewline(text));
 		}
 		else
 			return 1;
@@ -23,12 +19,7 @@ public class Calculator {
 
 	private static String[] splitNumbers(String numbers){
 	    
-		return numbers.split(",");
-	}
-	
-	private static String[] splitNewline(String numbers){
-	
-	return numbers.split("\n");
+		return numbers.split(",|\n");
 	}
       
     private static int sum(String[] numbers){
