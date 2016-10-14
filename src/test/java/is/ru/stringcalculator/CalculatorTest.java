263fa8bb51dca9c2b543caeffd30053893d65eba
Hjálmar Diego Arnórsson <hjalmar15@ru.is>
2016-10-14 09:01:02 +0000

    Test added for different delimiter

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -70,5 +70,9 @@ public class CalculatorTest {
     	assertEquals(2, Calculator.add("1001,2"));
     }
 	
+	@Test
+    public void differentDelimiter(){
+    	assertEquals(3, Calculator.add("//;\n1;2"));
+    }
 	
 }
